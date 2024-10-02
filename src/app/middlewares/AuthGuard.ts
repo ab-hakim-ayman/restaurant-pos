95549d5b451a9fc176cb1c8271b9fc103013d053
gjsoy24 <gour.joy24@gmail.com>
2024-10-02 17:24:56 +0600

    add user login route

---
+++ src/app/middlewares/AuthGuard.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+import AppError from '../errors/AppError';
+import { Admin } from '../modules/Admin/admin.model';
+import { User } from '../modules/User/user.model';
+import catchAsync from '../utils/catchAsync';
+
+const AuthGuard = (...requiredRoles: string[]) => {
+	return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+		const token = req.headers.authorization;
+		// check if the user send the token
+		if (!token) {
+			throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+		}
+
+		// check if the token is valid
+		let decoded;
+		try {
+			decoded = jwt.verify(token, config.jwt_secret) as JwtPayload;
+		} catch (error) {
+			throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+		}
+
+		const { id, email, role } = decoded;
+
+		// check if the user is exist. If not, throw an error
+		let user;
+		if (role === 'user') {
+			user = await User.findOne({ _id: id, email });
+		} else if (role === 'admin') {
+			user = await Admin.findOne({ _id: id, email });
+		} else {
+			throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+		}
+
+		if (!user) {
+			throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+		}
+
+		// check if the user has the required roles
+		if (requiredRoles.length && !requiredRoles.includes(user.role)) {
+			throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+		}
+
+		req.userData = user;
+		next();
+	});
+};
+
+export default AuthGuard;
